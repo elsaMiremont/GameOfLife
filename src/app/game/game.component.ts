e0b30a14ac5cc98f7b7c087ac5d4fd81d9cf3b7f
elsaMiremont <elsamiremont@gmail.com>
2021-03-06 02:36:04 +0100

    Init board, check cell state, count live cells around, draw cells, evolve gen WIP

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -7,13 +7,14 @@ import {Platform} from '@angular/cdk/platform';
   styleUrls: ['./game.component.scss']
 })
 export class GameComponent implements AfterViewInit {
-
+  // Injects a reference to the canvas
   @ViewChild('canvas', {static: true}) cnv: ElementRef<HTMLCanvasElement> | undefined;
-
+  // Set canvas width and height, initialize context
   width = window.innerWidth;
   height = window.innerHeight;
-  ctx: CanvasRenderingContext2D | null = null;
+  private ctx: CanvasRenderingContext2D | null = null;
 
+  // Declares DOM event listener on resizing the window to adapt width and height
   // tslint:disable-next-line:typedef
   @HostListener('window:resize', ['$event']) onResize(event: any) {
     this.width = window.innerWidth;
@@ -22,40 +23,110 @@ export class GameComponent implements AfterViewInit {
 
   constructor() {
   }
-
+  // Calling the content after Angular has fully initialized the component's view
   ngAfterViewInit(): void {
     if (!this.cnv) {
       return;
     }
+    // Get the canvas drawing context
     this.ctx = this.cnv.nativeElement.getContext('2d');
+
     // Will be used to increased or decreased grid size
     const tileSize = 20;
-    const tileX = this.width / tileSize;
-    const tileY = this.height / tileSize;
+    const tilesX = this.width / tileSize;
+    const tilesY = this.height / tileSize;
 
     // Canvas context styling
     if (!this.ctx) {
       return;
     }
     this.ctx.fillStyle = 'rgb(100, 240, 150)';
-    this.ctx.strokeStyle = 'rgb(200, 90, 90)';
-    this.ctx.lineWidth = 0.5;
+    this.ctx.strokeStyle = 'rgb(200, 200, 200)';
+    this.ctx.lineWidth = 1;
 
+    // Draw the grid on the board
     const drawGrid = () => {
       if (!this.ctx) {
         return;
       }
-
-      for ( let i = 0; i < tileX; i++ ) {
+      for ( let i = 0; i < tilesX; i++ ) {
         this.ctx.beginPath();
-        this.ctx.moveTo( i * tileSize - 0.5 * tileSize, 0);
-        this.ctx.lineTo( i * tileSize - 0.5 * tileSize, this.height);
+        this.ctx.moveTo(i * tileSize - 0.5, 0);
+        this.ctx.lineTo(i * tileSize - 0.5, this.height);
         this.ctx.stroke();
-        console.log(tileX);
-        console.log(tileY);
+      }
+      for ( let j = 0; j < tilesY; j++ ) {
+        this.ctx.beginPath();
+        this.ctx.moveTo(0, j * tileSize - 0.5);
+        this.ctx.lineTo(this.width, j * tileSize - 0.5);
+        this.ctx.stroke();
       }
     };
+    // Set the initial board to a boolean 2D array
+    const prepareBoard = (): boolean[][] => {
+      const board = [];
+      for ( let i = 0; i < tilesX; i++ ) {
+        const row = [];
+        for ( let j = 0; j < tilesY; j++ ) {
+          row.push(false);
+        }
+        board.push(row);
+      }
+      return board;
+    };
+    let BOARD = prepareBoard();
 
+    // Check if a cell is alive, also set to 0 all values outside the bounds
+    const isAlive = (x: number, y: number): number => {
+      if ( x < 0 || x >= tilesX || y < 0 || y >= tilesY ) {
+        return 0;
+      }
+      return BOARD[x][y] ? 1 : 0;
+    };
+    // Counts living neighbours around a cell
+    const suroundingCells = (x: number, y: number): number => {
+      let count = 0;
+      for (let i of [-1, 0, 1]) {
+        for (let j of [-1, 0, 1]) {
+          if (!(i === 0 && j === 0)) {
+            count += isAlive(x + 1, y + 1);
+          }
+        }
+      }
+      return count;
+    };
+    //
+    const drawBoard = () => {
+      for ( let i = 0; i < tilesX; i++ ) {
+        for ( let j = 0; j < tilesY; j++ ) {
+          if (!isAlive(i, j)) {
+            continue;
+          }
+          this.ctx?.fillRect(i * tileSize, j * tileSize, tileSize, tileSize);
+        }
+      }
+    };
+    /////////////// WIP
+    const evolveGeneration = (): boolean[][] => {
+      const board = prepareBoard();
+      for ( let i = 0; i < tilesX; i ++ ) {
+        for ( let j = 0; j < tilesY; j++) {
+          if (isAlive(i, j)) {
+            const count = suroundingCells(i, j);
+            if (count === 2 || count === 3) {
+              board[i][j] = true;
+            }
+          } else {
+            if ( suroundingCells(i, j) === 3) {
+              board[i][j] = true;
+            }
+          }
+        }
+      }
+      return board;
+    };
+
+    drawBoard();
     drawGrid();
   }
 }

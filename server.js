93ded3cb51351d3874adab7f87573ab0f31d3a0d
elsaMiremont <elsamiremont@gmail.com>
2021-03-08 02:55:38 +0100

    fix server

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(dirname + '/dist/game-of-life'));
+app.use(express.static(dirname + '/dist/GameOfLife'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(dirname+'/dist/game-of-life/index.html'));
+  res.sendFile(path.join(dirname+'/dist/GameOfLife/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
